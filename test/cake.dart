e06b3371ce1fdd0202c5ad17b439b3bb793a4182
afifi <moh.afifi93@gmail.com>
2020-09-15 14:19:06 +0200

    third commit

---
+++ test/cake.dart
@@ -0,0 +1,12 @@
+class MainCake{
+//non-private property
+//list of strings
+  List<String> randomPieceOfCakes = ['cake3', 'cake4', 'cake5', 'cake6'];
+
+  //private properties
+  var _pieceOfCake1 = "cake1";
+  String pieceOfCake2 = "cake2";
+  void getPieceOne(){
+    print(_pieceOfCake1);
+  }
+}
